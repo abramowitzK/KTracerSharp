6154ffaf489821338f819ef23e3e40a0015b9afe
Kyle Abramowitz <abramowitzk@gmail.com>
2016-04-09 10:46:06 -0400

    Initial commit. Also fuck c++

---
+++ KTracerSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//For Math library
+using OpenTK.Graphics;
+
+namespace KTracerSharp {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello, world!");
+		}
+	}
+}
