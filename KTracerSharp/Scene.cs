031e8db434cb2fa3ddb28e59060c05fb657aef1a
Kyle Abramowitz <abramowitzk@gmail.com>
2016-05-10 19:59:14 -0400

    changed default triangle num to 100

--- KTracerSharp/Scene.cs
+++ KTracerSharp/Scene.cs
@@ -7,7 +7,7 @@ using OpenTK;
 
 namespace KTracerSharp {
 	public class Scene {
-		public const int MaxTris = 1000;
+		public const int MaxTris = 100;
 		public Scene() {
 			Cam = new Camera(new Vector3(20.0f, 0.0f, 20.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector3(-1f, 0f, -1.0f), 45.0f,
 				10.0f);
